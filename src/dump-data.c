2985598a4e1962869b0006a2f233da4ba052c963
Aidan Kehoe <kehoea@parhasard.net>
2025-09-01 10:14:43 +0100

    Revert f38cf80e1163 of 2025-08-29, underlying problem was with ccache, not GCC
    src/ChangeLog addition:
    
    2025-09-01  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* dump-data.c:
    	Revert f38cf80e1163 of 2025-08-29, underlying problem was with
    	ccache, not with GCC, and so that change will not address the
    	issue if clang(1) is used. Thank you for the further
    	investigation, Mats Lidell.

--- src/dump-data.c
+++ src/dump-data.c
@@ -24,7 +24,7 @@ along with XEmacs.  If not, see <http://www.gnu.org/licenses/>. */
 #include "lisp.h"
 #include "dump-data.h"
 
-#if defined (__has_embed) && !(GCC_VERSION > NEED_GCC (15, 0, 0))
+#ifdef __has_embed
 #include <stdalign.h>
 
 alignas (16) static Rawbyte dumped_data[] = {
